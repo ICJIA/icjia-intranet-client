68e28e45cfec86c5265f4303af0ee46226467045
Chris Schweda <cschweda@gmail.com>
2020-06-05 14:33:41 -0500

    fix: add netlify redirects

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,8 @@
+//const path = require("path");
+
 module.exports = {
+  //publicPath:
+  //process.env.NODE_ENV === `production` ? appConfig.publicPath : "/",
+  publicPath: "/",
   transpileDependencies: ["vuetify"]
 };
