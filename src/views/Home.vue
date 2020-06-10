2c9f1d2bc086ca9bb96699f32b9d1618ca7f078b
Chris Schweda <cschweda@gmail.com>
2020-06-10 15:16:25 -0500

    fix: update home layout

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,7 +6,36 @@
           <Featured></Featured>
         </v-col>
       </v-row>
-      <v-row class="masonry">
+      <v-row class="mt-3">
+        <v-col
+          v-for="n in 4"
+          :key="n"
+          class="d-flex child-flex"
+          md="3"
+          xs="12"
+          sm="12"
+        >
+          <v-card class="mx-auto">
+            <v-card-text>
+              <div>Word of the Day</div>
+              <p class="display-1 text--primary">
+                be•nev•o•lent
+              </p>
+              <p>adjective</p>
+              <div class="text--primary">
+                well meaning and kindly.<br />
+                "a benevolent smile"
+              </div>
+            </v-card-text>
+            <v-card-actions>
+              <v-btn text color="blue accent-4">
+                Learn More
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
+      <v-row class="masonry mt-3">
         <v-col md="6" class="child">
           <v-sheet class="py-5 px-5" elevation="2">
             <h2
@@ -41,3 +70,9 @@
 </template>
 
 <script></script>
+
+<style>
+.v-card:not(.v-sheet--tile):not(.v-card--shaped) {
+  border-radius: 0px;
+}
+</style>
