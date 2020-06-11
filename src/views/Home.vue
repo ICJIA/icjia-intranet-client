a8d100ba86b0b7548eec15365f1f33466b989251
Chris Schweda <cschweda@gmail.com>
2020-06-11 09:51:55 -0500

    fix: adjust home styling

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
     <v-container fluid>
-      <v-row>
+      <v-row class="mt-2">
         <v-col>
           <Featured></Featured>
         </v-col>
       </v-row>
-      <v-row class="mt-3">
+      <v-row class="mt-6">
         <v-col
           v-for="n in 2"
           :key="n"
@@ -15,42 +15,44 @@
           xs="12"
           sm="12"
         >
-          <v-card class="mx-auto" color="white">
+          <v-card dark class="mx-auto px-2 py-5" style="background: #225b89;">
             <v-card-text>
               <div>Lorem markdownum</div>
-              <p class="display-1 text--primary">
-                rogat reminiscitur
-              </p>
-              <p>adjective</p>
-              <div class="text--primary">
-                Placuit manet deceptus, ira duris iuncta, rogat reminiscitur
+              <h2 style="font-size: 24px; color: #fff;" class="mt-3 mb-4">
+                Request ICJIA Resources
+              </h2>
+
+              <div style="color: #fff;">
+                Request a conference room, laptop, or a rental car.
               </div>
             </v-card-text>
-            <v-card-actions>
-              <v-btn text style="color: #0d4474;">
-                Learn More
-              </v-btn>
-            </v-card-actions>
+
+            <v-btn absolute dark fab bottom right color="blue darken-3">
+              <v-icon>more_vert</v-icon>
+            </v-btn>
           </v-card>
         </v-col>
       </v-row>
-      <v-row class="mt-3">
+      <v-row class="mt-6">
         <v-col sm="12" md="6" cols="12" class="child">
-          <v-sheet class="py-5 px-5" elevation="2">
-            <v-container fluid class="mb-8">
-              <v-row>
-                <v-col>
-                  <h2 style="color: #0d4474;">
-                    News & Updates
-                  </h2>
-                </v-col>
-                <v-col class="text-right mt-2">
-                  <v-btn dark x-small color="indigo darken-3" to="/news"
-                    >news archive</v-btn
-                  >
-                </v-col>
-              </v-row>
-              <div class="mt-3" style="border-bottom: 2px solid #0d4474;"></div>
+          <v-sheet class="px-2 py-2" elevation="2" style="">
+            <v-container fluid class="mb-8" style="margin: 0; padding: 0;">
+              <div style="background: #ccc;" class="px-3 py-3">
+                <v-row no-gutters>
+                  <v-col>
+                    <h2 style="color: #333;">
+                      News & Updates
+                    </h2>
+                  </v-col>
+                  <v-col class="text-right mt-2">
+                    <v-btn outlined x-small color="black" to="/news"
+                      >news archive&nbsp;<v-icon right x-small
+                        >calendar_today</v-icon
+                      ></v-btn
+                    >
+                  </v-col>
+                </v-row>
+              </div>
             </v-container>
 
             <HomePosts></HomePosts>
@@ -58,21 +60,24 @@
         </v-col>
 
         <v-col sm="12" md="6" cols="12">
-          <v-sheet class="py-5 px-5" elevation="0">
-            <v-container fluid class="mb-8">
-              <v-row>
-                <v-col>
-                  <h2 style="color: #0d4474;">
-                    Events
-                  </h2>
-                </v-col>
-                <v-col class="text-right mt-2">
-                  <v-btn dark x-small color="indigo darken-3" to="/events"
-                    >calendar</v-btn
-                  >
-                </v-col>
-              </v-row>
-              <div class="mt-3" style="border-bottom: 2px solid #0d4474;"></div>
+          <v-sheet class="py-2 px-2" elevation="0">
+            <v-container fluid class="mb-8" style="margin: 0; padding: 0;">
+              <div style="background: #ccc;" class="px-3 py-3">
+                <v-row no-gutters>
+                  <v-col>
+                    <h2 style="color: #333;">
+                      Upcoming Events
+                    </h2>
+                  </v-col>
+                  <v-col class="text-right mt-2">
+                    <v-btn outlined x-small color="black" to="/news"
+                      >all events&nbsp;<v-icon right x-small
+                        >calendar_today</v-icon
+                      ></v-btn
+                    >
+                  </v-col>
+                </v-row>
+              </div>
             </v-container>
             <HomeEvents></HomeEvents>
           </v-sheet>
