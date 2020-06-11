a0cbc8af2f8f6d21ff78179700c8708defec341d
Chris Schweda <cschweda@gmail.com>
2020-06-11 13:10:47 -0500

    fix: adjust home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -12,17 +12,20 @@
           :key="n"
           class="d-flex child-flex"
           md="6"
-          xs="12"
-          sm="12"
+          cols="12"
         >
-          <v-card dark class="mx-auto px-2 py-5" style="background: #225b89;">
+          <v-card
+            class="mx-auto px-2 py-5"
+            style="background: #ccc;"
+            elevation="8"
+          >
             <v-card-text>
               <div>Lorem markdownum</div>
-              <h2 style="font-size: 24px; color: #fff;" class="mt-3 mb-4">
+              <h2 style="font-size: 24px; color: #333;" class="mt-3 mb-4">
                 Request ICJIA Resources
               </h2>
 
-              <div style="color: #fff;">
+              <div style="color: #333;">
                 Request a conference room, laptop, or a rental car.
               </div>
             </v-card-text>
@@ -35,17 +38,17 @@
       </v-row>
       <v-row class="mt-6">
         <v-col sm="12" md="6" cols="12" class="child">
-          <v-sheet class="px-2 py-2" elevation="2" style="">
+          <v-sheet class="px-2 py-2" elevation="8" style="">
             <v-container fluid class="mb-8" style="margin: 0; padding: 0;">
-              <div style="background: #ccc;" class="px-3 py-3">
+              <div style="background: #225b89;" class="px-3 py-3">
                 <v-row no-gutters>
                   <v-col>
-                    <h2 style="color: #333;">
+                    <h2 style="color: #fff;">
                       News & Updates
                     </h2>
                   </v-col>
                   <v-col class="text-right mt-2">
-                    <v-btn outlined x-small color="black" to="/news"
+                    <v-btn outlined x-small color="white" to="/news"
                       >news archive&nbsp;<v-icon right x-small
                         >calendar_today</v-icon
                       ></v-btn
@@ -60,17 +63,17 @@
         </v-col>
 
         <v-col sm="12" md="6" cols="12">
-          <v-sheet class="py-2 px-2" elevation="0">
+          <v-sheet class="py-2 px-2" elevation="8">
             <v-container fluid class="mb-8" style="margin: 0; padding: 0;">
-              <div style="background: #ccc;" class="px-3 py-3">
+              <div style="background: #225b89;" class="px-3 py-3">
                 <v-row no-gutters>
                   <v-col>
-                    <h2 style="color: #333;">
+                    <h2 style="color: #fff;">
                       Upcoming Events
                     </h2>
                   </v-col>
                   <v-col class="text-right mt-2">
-                    <v-btn outlined x-small color="black" to="/news"
+                    <v-btn outlined x-small color="white" to="/news"
                       >all events&nbsp;<v-icon right x-small
                         >calendar_today</v-icon
                       ></v-btn
