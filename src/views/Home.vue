0fbc8575bb5fbbd36bfa232c61fd2743a9608ef5
Chris Schweda <cschweda@gmail.com>
2020-09-02 10:04:11 -0500

    fix: prepare home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,113 +1,110 @@
 <template>
-  <div>
-    <v-container fluid>
-      <v-row class="" style="margin-top: -5px;">
-        <v-col>
-          <Featured></Featured>
-        </v-col>
-      </v-row>
-      <v-row class="mt-6">
-        <v-col>
-          <HomeQuickBoxes></HomeQuickBoxes>
-        </v-col>
-      </v-row>
-      <v-row class="mt-6">
-        <v-col sm="12" md="6" cols="12" class="child">
-          <v-sheet class="px-2 py-2" elevation="1" style="">
-            <v-container fluid class="mb-8" style="margin: 0; padding: 0;">
-              <div style="background: #225b89;" class="px-3 py-3">
-                <v-row no-gutters>
-                  <v-col>
-                    <h2 style="color: #fff;">
-                      News & Updates
-                    </h2>
-                  </v-col>
-                  <v-col class="text-right mt-1">
-                    <v-btn outlined small color="white" to="/news"
-                      >news archive&nbsp;<v-icon right small
-                        >view_headline</v-icon
-                      ></v-btn
-                    >
-                  </v-col>
-                </v-row>
-              </div>
-            </v-container>
-            <v-container style="min-height: 500px;">
-              <v-row>
-                <v-col>
-                  <HomePosts></HomePosts>
-                </v-col>
-              </v-row>
-            </v-container>
-          </v-sheet>
-        </v-col>
+  <ApolloQuery
+    :query="GET_HOME"
+    notifyOnNetworkStatusChange
+    :variables="{ now, eventLimit, postLimit }"
+    @result="afterFetch"
+  >
+    <template slot-scope="{ result }">
+      <div v-if="isLoading(result.loading)">
+        <Loader></Loader>
+      </div>
 
-        <v-col sm="12" md="6" cols="12">
-          <v-sheet class="py-2 px-2" elevation="1">
-            <v-container fluid class="mb-8" style="margin: 0; padding: 0;">
-              <div style="background: #225b89;" class="px-3 py-3">
-                <v-row no-gutters>
-                  <v-col>
-                    <h2 style="color: #fff;">
-                      Upcoming Events
-                    </h2>
-                  </v-col>
-                  <v-col class="text-right mt-1">
-                    <v-btn outlined small color="white" to="/calendar"
-                      >calendar&nbsp;<v-icon right small>today</v-icon></v-btn
-                    >
-                  </v-col>
-                </v-row>
-              </div>
-            </v-container>
-            <v-container style="min-height: 500px;">
-              <v-row>
-                <v-col>
-                  <HomeEvents></HomeEvents>
-                </v-col>
-              </v-row>
-            </v-container>
-          </v-sheet>
-        </v-col>
-      </v-row>
-      <v-row>
-        <v-col cols="12">
-          <v-sheet class="py-2 px-2" elevation="1">
-            <v-container fluid class="mb-8" style="margin: 0; padding: 0;">
-              <div style="background: #225b89;" class="px-3 py-3">
-                <v-row no-gutters>
-                  <v-col>
-                    <h2 style="color: #fff;">
-                      Latest ICJIA Research
-                    </h2>
-                  </v-col>
-                  <v-col class="text-right mt-1">
-                    <v-btn
-                      outlined
-                      small
-                      color="white"
-                      href="https://icjia.illinois.gov/researchhub"
-                      target="_blank"
-                      >ResearchHub&nbsp;<v-icon right small
-                        >open_in_new</v-icon
-                      ></v-btn
-                    >
-                  </v-col>
-                </v-row>
-              </div>
-            </v-container>
-            <Research></Research>
-          </v-sheet>
-        </v-col>
-      </v-row>
-    </v-container>
-  </div>
+      <div v-if="!isLoading(result.loading) && !result.error">
+        <!-- START: home components -->
+
+        <HomeSlider
+          :slides="result.data.home.slider"
+          v-if="result.data.home.slider"
+        ></HomeSlider>
+
+        <HomeQuickMenus
+          :menus="result.data.home.quick_menu"
+          v-if="result.data.home.quick_menu"
+        ></HomeQuickMenus>
+
+        <h2>Posts</h2>
+        {{ result.data.posts }}
+        <br />
+
+        <HomeEvents
+          :events="mergedEvents"
+          v-if="result.data.events && result.data.eventRange"
+        ></HomeEvents>
+
+        <!-- END: home components -->
+      </div>
+      <div v-if="result.error" class="text-center error apollo">
+        {{ result.error }}
+      </div>
+      <Research></Research>
+    </template>
+  </ApolloQuery>
 </template>
+<script>
+import { GET_HOME } from "@/graphql/queries/home.js";
+// eslint-disable-next-line no-unused-vars
+import _ from "lodash";
+import moment from "moment";
+// eslint-disable-next-line no-unused-vars
+import tz from "moment-timezone";
+export default {
+  name: "Home",
+  components: {},
+  computed: {
+    name() {
+      return this.data;
+    },
+  },
+  created() {
+    this.now = moment().tz(this.$myApp.config.timezone).format("YYYY-MM-DD");
+  },
+  mounted() {
+    console.log(this.$myApp.config.timezone);
+  },
+  data() {
+    return {
+      GET_HOME,
+      now: null,
+      mergedEvents: () => [],
+      eventLimit: this.$myApp.config.home.eventLimit,
+      postLimit: this.$myApp.config.home.postLimit,
+    };
+  },
+  methods: {
+    isLoading(loading) {
+      // eslint-disable-next-line no-undef
+      loading ? NProgress.start() : NProgress.done();
+      return loading ? true : false;
+    },
 
-<script></script>
+    afterFetch(result) {
+      if (result.data && result.data.events && result.data.eventRange) {
+        let mergedEvents = [...result.data.events, ...result.data.eventRange];
+        mergedEvents = _.sortBy(mergedEvents, (o) => o.start);
+        this.mergedEvents = mergedEvents.slice(0, this.eventLimit);
+      }
+    },
+  },
+};
+</script>
 
 <style>
-.v-card:not(.v-sheet--tile):not(.v-card--shaped) {
-  border-radius: 0px !important;
+.hover {
+  cursor: pointer;
+}
+.card:hover {
+  box-shadow: 0px 0px 15px #000000;
+  z-index: 2;
+  -webkit-transition: all 100ms ease-in;
+  -webkit-transform: scale(1.01);
+  -ms-transition: all 100ms ease-in;
+  -ms-transform: scale(1.01);
+  -moz-transition: all 100ms ease-in;
+  -moz-transform: scale(1.01);
+  transition: all 100ms ease-in;
+  transform: scale(1.01);
+  cursor: pointer;
+  background: #fafafa;
 }
 </style>
