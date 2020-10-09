e7b3fb4d73e242987b1fb53cbf062d9fe6d088cf
Chris Schweda <cschweda@gmail.com>
2020-10-09 14:58:22 -0500

    fix: TOC scroll

--- src/components/Toc.vue
+++ src/components/Toc.vue
@@ -1,24 +1,38 @@
 <template>
-  <div class="ml-5">
-    <div class="mb-3" style="font-weight: bold">NAVIGATION</div>
-    <ul class="hover toc">
-      <span v-for="(item, index) in toc" :key="index">
-        <li v-if="!item.parent" @click="scrollTo(`${item.target}`)">
-          <span :id="`scrollTo-${item.id}`" class="tocItem tocParent">{{
+  <div
+    class="toc"
+    :class="{
+      'pl-2': $vuetify.breakpoint.xs || $vuetify.breakpoint.xs,
+      'pl-10':
+        $vuetify.breakpoint.md ||
+        $vuetify.breakpoint.lg ||
+        $vuetify.breakpoint.xl,
+      shaded: $vuetify.breakpoint.xs || $vuetify.breakpoint.xs,
+    }"
+    style=""
+  >
+    <div
+      style="margin-left: -3px; font-weight: bold"
+      ref="anchor"
+      class="mb-4 hover anchor"
+      @click="$vuetify.goTo(0)"
+      v-if="tocHeading.length"
+    >
+      {{ tocHeading | upperCase }}
+    </div>
+    <div class="divider">
+      <ul class="toc-list">
+        <li
+          v-for="(item, index) in toc"
+          :key="index"
+          @click="scrollTo(item.id)"
+        >
+          <span :id="`scrollTo-${item.id}`" class="tocItem">{{
             item.text
           }}</span>
         </li>
-        <li v-else @click="scrollTo(`${item.target}`)">
-          <ul class="toc">
-            <li>
-              <span :id="`scrollTo-${item.id}`" class="tocItem tocParent">{{
-                item.text
-              }}</span>
-            </li>
-          </ul>
-        </li>
-      </span>
-    </ul>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -29,32 +43,79 @@ export default {
       toc: [],
     };
   },
-  async mounted() {
-    await this.setToc();
+  props: {
+    selector: {
+      type: String,
+      default: "#scrollArea",
+    },
+    tocHeading: {
+      type: String,
+      default: "NAVIGATION",
+    },
+    top: {
+      type: String,
+      default: "#baseContentTop",
+    },
+    mini: {
+      type: Boolean,
+      default: false,
+    },
   },
   methods: {
     scrollTo(id) {
       //console.log(id);
-      this.$vuetify.goTo(`#${id}`, { offset: 10 });
+      this.$vuetify.goTo(`#${id}`, { offset: 20 });
     },
     setToc() {
-      const sections = Array.from(document.querySelectorAll("h2, h3"));
-
-      let parent;
+      const sections = Array.from(document.querySelectorAll("h2"));
       sections.forEach((section) => {
         let obj = {};
         obj.text = section.innerText;
-        obj.target = section.id;
-        obj.nodeName = section.nodeName;
-        if (section.nodeName === "H2") {
-          parent = section.id;
-          obj.parent = null;
-        } else {
-          obj.parent = parent;
-        }
+        obj.id = section.id;
         this.toc.push(obj);
       });
     },
+  },
+  async mounted() {
+    await this.setToc();
+    var section = document.querySelectorAll("h2");
+    if (section) {
+      var sections = {};
+      var i = 0;
+      this.$refs["anchor"].classList.add("visible");
+      section.forEach((e) => {
+        sections[e.id] = e.offsetTop - 50;
+      });
+
+      window.onscroll = () => {
+        var scrollPosition =
+          document.documentElement.scrollTop || document.body.scrollTop;
+        const tocItems = document.querySelectorAll(".tocItem");
+
+        if (scrollPosition < 100) {
+          tocItems.forEach((toc) => {
+            toc.classList.remove("visible");
+          });
+          this.$refs["anchor"].classList.add("visible");
+        } else {
+          this.$refs["anchor"].classList.remove("visible");
+        }
+        //console.log(scrollPosition);
+        for (i in sections) {
+          if (sections[i] <= scrollPosition) {
+            const sectionItem = document.getElementById(`scrollTo-${i}`);
+
+            tocItems.forEach((toc) => {
+              toc.classList.remove("visible");
+            });
+            sectionItem.classList.add("visible");
+          }
+        }
+      };
+    }
+  },
+  beforeDestroy() {
+    window.onscroll = () => {};
   },
 };
 </script>
@@ -65,22 +126,51 @@ export default {
 }
 
 .visible {
-  color: #116bb9 !important;
+  color: #0d4474;
   font-weight: bold;
 }
-ul.toc {
-  list-style: none;
+.anchor {
+  padding: 2px 5px 2px 5px;
+}
+.anchor:hover {
+  color: #0d4474;
+  background: #eee;
 }
 
-ul.toc li {
-  margin-left: -10px;
+.shaded {
+  background: #eee;
+  padding: 15px;
+  margin-bottom: 25px;
 }
 
-li .tocItem:hover {
+.toc {
+}
+
+ul.toc-list {
+  list-style-type: none;
+}
+
+ul.toc-list li {
+  color: #333;
+
+  padding: 2px 5px 2px 5px;
+  cursor: pointer;
+  margin-bottom: 3px;
+  font-size: 14px;
+}
+
+ul.toc-list li:hover {
   color: #aaa;
+  background: #eee;
 }
+@media only screen and (max-width: 1024px) {
+  .divider {
+    border-left: 0px solid #ccc;
+  }
 
-.tocItem {
-  font-weight: 700;
+  ul.toc-list li:hover {
+    color: #fff;
+    background: #aaa;
+  }
 }
 </style>
