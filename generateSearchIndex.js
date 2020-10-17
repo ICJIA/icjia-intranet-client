2454ff0255043b14dee47603d7b0e66499010c80
Chris Schweda <cschweda@gmail.com>
2020-10-17 11:49:48 -0500

    fix: drafting searchIndex structure

--- generateSearchIndex.js
+++ generateSearchIndex.js
@@ -10,9 +10,23 @@ const dotenv = require("dotenv").config();
 const JWT = dotenv.parsed.JWT;
 const endpoint = config.api.baseGraphQL;
 
+const md = require("markdown-it")({
+  html: true,
+  xhtmlOut: false,
+  breaks: false,
+  langPrefix: "language-",
+  linkify: false,
+  typographer: false,
+  quotes: "“”‘’",
+  highlight: function (/*str, lang*/) {
+    return "";
+  },
+})
+  .use(require("markdown-it-attrs"))
+  .use(require("markdown-it-named-headers"));
+
 const { GraphQLClient, gql } = require("graphql-request");
 const { Headers } = require("cross-fetch");
-
 global.Headers = global.Headers || Headers;
 
 const graphqlClient = new GraphQLClient(endpoint, {
@@ -21,20 +35,37 @@ const graphqlClient = new GraphQLClient(endpoint, {
   },
 });
 
+const sections = ["posts", "events"];
+
 const query = gql`
   query {
     posts {
+      id
       title
       summary
       searchMeta
       body
+    }
+    events {
+      id
+      name
+      type
+      summary
+      searchMeta
+      details
     }
   }
 `;
 
 async function main() {
   const data = await graphqlClient.request(query);
-  console.log(data);
+  const sections = Object.keys(data);
+  //console.log(data.posts);
+  let index = sections.map((section) => {
+    let items = data[section].map((item) => {
+      console.log(item.id);
+    });
+  });
 }
 
 main();
