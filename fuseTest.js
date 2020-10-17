7b08daabd9bcd09ba567a6557a429f6be5bad5e0
Chris Schweda <cschweda@gmail.com>
2020-10-17 15:53:43 -0500

    fix: refactor test

--- fuseTest.js
+++ fuseTest.js
@@ -1,28 +1,13 @@
 const Fuse = require("fuse.js");
-
-const options = {
-  isCaseSensitive: false,
-  includeScore: true,
-  shouldSort: true,
-  includeMatches: true,
-  findAllMatches: true,
-  minMatchCharLength: 1,
-  location: 0,
-  threshold: 0.8,
-  distance: 100,
-  useExtendedSearch: false,
-  ignoreLocation: false,
-  ignoreFieldNorm: false,
-  keys: ["searchMeta", "summary", "title", "headings.text"],
-};
+const query = "Integer ac leo";
 
 (async function main() {
+  const searchOptions = await require("./src/config.json").search;
   const searchIndex = await require("./public/searchIndex.json");
-  const myIndex = Fuse.createIndex(options.keys, searchIndex);
-  const fuse = new Fuse(searchIndex, options, myIndex);
-  const pattern = "Integer ac leo";
-  const result = fuse.search(pattern).slice(0, 2);
+  const myIndex = Fuse.createIndex(searchOptions.keys, searchIndex);
+  const fuse = new Fuse(searchIndex, searchOptions, myIndex);
+  const result = fuse.search(query).slice(0, 2);
 
-  console.log("pattern: ", pattern);
+  console.log("query: ", query);
   console.log(result);
 })();
