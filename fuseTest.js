d53ff823246b54a99101bbba20d8eda72f6891ea
Chris Schweda <cschweda@gmail.com>
2020-10-17 15:46:50 -0500

    fix: cleanup

--- fuseTest.js
+++ fuseTest.js
@@ -16,22 +16,13 @@ const options = {
   keys: ["searchMeta", "summary", "title", "headings.text"],
 };
 
-// Change the pattern
-// const pattern = ""
-
-// return fuse.search(pattern)
-
-async function main() {
+(async function main() {
   const searchIndex = await require("./public/searchIndex.json");
-  // console.log("main invoked");
   const myIndex = Fuse.createIndex(options.keys, searchIndex);
   const fuse = new Fuse(searchIndex, options, myIndex);
-  // Change the pattern
   const pattern = "Integer ac leo";
-
   const result = fuse.search(pattern).slice(0, 2);
+
   console.log("pattern: ", pattern);
   console.log(result);
-}
-
-main();
+})();
