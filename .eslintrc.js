16df760d680b89534a1187e5c8eecc94708bd39b
Chris Schweda <cschweda@gmail.com>
2021-01-05 10:44:41 -0600

    fix: update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,7 @@ module.exports = {
 
   rules: {
     "vue/experimental-script-setup-vars": "off",
+    "vue/valid-v-slot": "off",
     "no-console": process.env.NODE_ENV === "production" ? "off" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "off" : "off",
     "graphql/template-strings": [
