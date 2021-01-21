457d61e454d631f42b049c50905c65c8992e23a7
Chris Schweda <cschweda@gmail.com>
2021-01-21 12:26:34 -0600

    fix: entry for IE

--- src/entry.js
+++ src/entry.js
@@ -1,16 +1,14 @@
-let browserIsIE =
-  navigator.userAgent.toUpperCase().indexOf("TRIDENT/") != -1 ||
-  navigator.userAgent.toUpperCase().indexOf("MSIE") != -1;
-
-if (browserIsIE)
-  document.querySelector("#app").innerHTML = createBanner("ICJIA Intranet");
+if (isIE(window.navigator.userAgent))
+  document.querySelector("#app").innerHTML = createBanner(
+    "R3: Restore. Reinvest. Renew."
+  );
 else require("./main");
 
-// function isIE(ua) {
-//   const isIE10orLess = ua.indexOf("MSIE") > -1;
-//   const isIE11 = ua.indexOf("Trident/") > -1;
-//   return isIE10orLess || isIE11;
-// }
+function isIE(ua) {
+  const isIE10orLess = ua.indexOf("MSIE") > -1;
+  const isIE11 = ua.indexOf("Trident/") > -1;
+  return isIE10orLess || isIE11;
+}
 
 function createBanner() {
   // const baseURL = process.env.BASE_URL;
@@ -19,17 +17,20 @@ function createBanner() {
   <div style="text-align: center; font-size: 0.7em;">
     
     <h1 style="font-weight: 900; font-size: 36px; margin-top: 100px; margin-bottom: 32px; font-family: 'Roboto', sans-serif !important;">
-          ICJIA Intranet
+          R<span style="color: #ccaa41">3</span>&nbsp;<span
+            >RESTORE. REINVEST. RENEW.</span
+          >
         </h1>
 
     <h1 style='font-family: Lato, sans-serif; margin-top: 50px; padding-top: 20px; padding-bottom:20px; background: #eee;'>
-      The ICJIA Intranet site does not support Internet Explorer.
+      The R3 site does not support Internet Explorer.
       <br><br>
       Please upgrade to a modern, secure browser such as
       <a href='https://www.google.com/chrome/'>Chrome</a>,
       <a href='https://www.mozilla.org/en-US/firefox/new/'>Firefox</a>, or
       <a href='https://www.microsoft.com/en-us/windows/microsoft-edge'>MS Edge</a>.
     </h1>
-   
+    <h2 style="margin-top: 50px;">For additional information about R3 opportunities, please see the Illinois Criminal Justice Information Authority's 
+    <a href="https://icjia.illinois.gov/gata">GATA site</a>.</h2>
   </div>`;
 }
