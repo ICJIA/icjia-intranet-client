5611a8d195710956b331842e295096d829933008
Chris Schweda <cschweda@gmail.com>
2021-02-25 12:04:43 -0600

    fix: add v-chip for tag

--- src/components/Tags.vue
+++ src/components/Tags.vue
@@ -2,7 +2,10 @@
   <div style="font-size: 10px" class="pt-4">
     <!-- <span v-if="showText">Tags:&nbsp;&nbsp;</span> -->
     <span v-for="(tag, index) in tags" :key="index">
-      <router-link :to="`/tags/${tag.slug}/`">{{ tag.title }}</router-link
+      <v-chip x-small>
+        <router-link :to="`/tags/${tag.slug}/`">{{
+          tag.title
+        }}</router-link> </v-chip
       ><span v-if="index != tags.length - 1">,</span>
     </span>
   </div>
