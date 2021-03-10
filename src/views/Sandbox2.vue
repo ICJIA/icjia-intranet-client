9ee083810f9db50670c9854a19295da2e64f1f94
cschweda <cschweda@gmail.com>
2021-03-10 16:15:16 -0600

    fix: adjust clap layout

--- src/views/Sandbox2.vue
+++ src/views/Sandbox2.vue
@@ -1,32 +1,39 @@
 <template>
   <div class="text-center" style="margin-top: 100px">
-    <button id="clap" class="clap">
-      <span>
-        <!--  SVG Created by Luis Durazo from the Noun Project  -->
-        <svg
-          id="clap--icon"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="-549 338 100.1 125"
-        >
-          <path
-            d="M-471.2 366.8c1.2 1.1 1.9 2.6 2.3 4.1.4-.3.8-.5 1.2-.7 1-1.9.7-4.3-1-5.9-2-1.9-5.2-1.9-7.2.1l-.2.2c1.8.1 3.6.9 4.9 2.2zm-28.8 14c.4.9.7 1.9.8 3.1l16.5-16.9c.6-.6 1.4-1.1 2.1-1.5 1-1.9.7-4.4-.9-6-2-1.9-5.2-1.9-7.2.1l-15.5 15.9c2.3 2.2 3.1 3 4.2 5.3zm-38.9 39.7c-.1-8.9 3.2-17.2 9.4-23.6l18.6-19c.7-2 .5-4.1-.1-5.3-.8-1.8-1.3-2.3-3.6-4.5l-20.9 21.4c-10.6 10.8-11.2 27.6-2.3 39.3-.6-2.6-1-5.4-1.1-8.3z"
-          />
-          <path
-            d="M-527.2 399.1l20.9-21.4c2.2 2.2 2.7 2.6 3.5 4.5.8 1.8 1 5.4-1.6 8l-11.8 12.2c-.5.5-.4 1.2 0 1.7.5.5 1.2.5 1.7 0l34-35c1.9-2 5.2-2.1 7.2-.1 2 1.9 2 5.2.1 7.2l-24.7 25.3c-.5.5-.4 1.2 0 1.7.5.5 1.2.5 1.7 0l28.5-29.3c2-2 5.2-2 7.1-.1 2 1.9 2 5.1.1 7.1l-28.5 29.3c-.5.5-.4 1.2 0 1.7.5.5 1.2.4 1.7 0l24.7-25.3c1.9-2 5.1-2.1 7.1-.1 2 1.9 2 5.2.1 7.2l-24.7 25.3c-.5.5-.4 1.2 0 1.7.5.5 1.2.5 1.7 0l14.6-15c2-2 5.2-2 7.2-.1 2 2 2.1 5.2.1 7.2l-27.6 28.4c-11.6 11.9-30.6 12.2-42.5.6-12-11.7-12.2-30.8-.6-42.7m18.1-48.4l-.7 4.9-2.2-4.4m7.6.9l-3.7 3.4 1.2-4.8m5.5 4.7l-4.8 1.6 3.1-3.9"
-          />
-        </svg>
-      </span>
-      <span id="clap--count" class="clap--count"></span>
-      <span id="clap--count-total" class="clap--count-total"></span>
-    </button>
-    <div
-      v-if="clapCount > 0"
-      style="font-size: 10px; margin-left: -10px"
-      class="mt-5"
-    >
-      You've given this page {{ clapCount }} clap{{
-        clapCount > 1 ? "s" : null
-      }}.{{ clapCount >= clapMax ? " You're maxed out. Thank you!" : null }}
+    <div style="position: relative">
+      <!-- <div
+        v-if="startup"
+        style="font-size: 12px; margin-bottom: 10px; color: #888; position: absolute"
+      >
+        {{ clapTotalCount }}
+      </div> -->
+
+      <button id="clap" class="clap">
+        <span>
+          <!--  SVG Created by Luis Durazo from the Noun Project  -->
+          <svg
+            id="clap--icon"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="-549 338 100.1 125"
+          >
+            <path
+              d="M-471.2 366.8c1.2 1.1 1.9 2.6 2.3 4.1.4-.3.8-.5 1.2-.7 1-1.9.7-4.3-1-5.9-2-1.9-5.2-1.9-7.2.1l-.2.2c1.8.1 3.6.9 4.9 2.2zm-28.8 14c.4.9.7 1.9.8 3.1l16.5-16.9c.6-.6 1.4-1.1 2.1-1.5 1-1.9.7-4.4-.9-6-2-1.9-5.2-1.9-7.2.1l-15.5 15.9c2.3 2.2 3.1 3 4.2 5.3zm-38.9 39.7c-.1-8.9 3.2-17.2 9.4-23.6l18.6-19c.7-2 .5-4.1-.1-5.3-.8-1.8-1.3-2.3-3.6-4.5l-20.9 21.4c-10.6 10.8-11.2 27.6-2.3 39.3-.6-2.6-1-5.4-1.1-8.3z"
+            />
+            <path
+              d="M-527.2 399.1l20.9-21.4c2.2 2.2 2.7 2.6 3.5 4.5.8 1.8 1 5.4-1.6 8l-11.8 12.2c-.5.5-.4 1.2 0 1.7.5.5 1.2.5 1.7 0l34-35c1.9-2 5.2-2.1 7.2-.1 2 1.9 2 5.2.1 7.2l-24.7 25.3c-.5.5-.4 1.2 0 1.7.5.5 1.2.5 1.7 0l28.5-29.3c2-2 5.2-2 7.1-.1 2 1.9 2 5.1.1 7.1l-28.5 29.3c-.5.5-.4 1.2 0 1.7.5.5 1.2.4 1.7 0l24.7-25.3c1.9-2 5.1-2.1 7.1-.1 2 1.9 2 5.2.1 7.2l-24.7 25.3c-.5.5-.4 1.2 0 1.7.5.5 1.2.5 1.7 0l14.6-15c2-2 5.2-2 7.2-.1 2 2 2.1 5.2.1 7.2l-27.6 28.4c-11.6 11.9-30.6 12.2-42.5.6-12-11.7-12.2-30.8-.6-42.7m18.1-48.4l-.7 4.9-2.2-4.4m7.6.9l-3.7 3.4 1.2-4.8m5.5 4.7l-4.8 1.6 3.1-3.9"
+            />
+          </svg>
+        </span>
+        <span id="clap--count" class="clap--count"></span>
+        <span id="clap--count-total" class="clap--count-total">{{
+          clapTotalCount
+        }}</span>
+      </button>
+      <div v-if="clapCount > 0" style="font-size: 10px" class="mt-5">
+        You've given this page {{ clapCount }} clap{{
+          clapCount > 1 ? "s" : null
+        }}.{{ clapCount >= clapMax ? " You're maxed out. Thank you!" : null }}
+      </div>
     </div>
   </div>
 </template>
@@ -45,6 +52,7 @@ export default {
       initialNumberOfClaps: null,
       clapMax: 15,
       clapTotalCount: null,
+      startup: true,
     };
   },
   methods: {
@@ -116,6 +124,7 @@ export default {
     const clapIcon = document.getElementById("clap--icon");
     const clapCount = document.getElementById("clap--count");
     const clapTotalCount = document.getElementById("clap--count-total");
+
     //const initialNumberOfClaps = this.initialNumberOfClaps;
     const clapMax = 15;
     const tlDuration = 300;
@@ -201,17 +210,28 @@ export default {
       scaleButton,
     ]);
 
+    const startupTimeline = new window.mojs.Timeline();
+    startupTimeline.add([
+      triangleBurst,
+      circleBurst,
+      countTotalAnimation,
+      scaleButton,
+    ]);
+
     clap.addEventListener("click", function () {
+      vm.startup = false;
       repeatClapping();
     });
 
     clap.addEventListener("mousedown", function () {
       clapHold = setInterval(function () {
+        vm.startup = false;
         repeatClapping();
       }, 400);
     });
 
     clap.addEventListener("mouseup", function () {
+      vm.startup = false;
       clearInterval(clapHold);
     });
 
@@ -231,7 +251,8 @@ export default {
       console.log("initial: ", vm.initialNumberOfClaps);
       numberOfClaps++;
       vm.clapCount = numberOfClaps;
-      clapCount.innerHTML = "+" + numberOfClaps;
+      //clapCount.innerHTML = "+" + numberOfClaps;
+      clapCount.innerHTML = "+1";
       clapTotalCount.innerHTML = vm.initialNumberOfClaps++;
       console.log(clapTotalCount.innerHTML);
       localStorage.setItem(vm.pageID, numberOfClaps);
@@ -243,6 +264,7 @@ export default {
       let res = await updateClapEntry(vm.$store.state.auth.jwt, dbObj, vm.id);
       console.log("dbInsert: ", res);
     }
+    startupTimeline.replay();
   },
 };
 </script>
@@ -317,38 +339,8 @@ $secondary-color: rgba(39, 174, 96, 1);
     width: $btn-dimension;
     text-align: center;
     left: 0;
-    top: -$btn-dimension/3.5;
+    top: -$btn-dimension/2.5;
     color: $primary-color;
-  }
-}
-
-/*====================
-        Message
-======================*/
-#message {
-  position: absolute;
-  bottom: 20px;
-  color: $secondary-color;
-  line-height: 1.52rem;
-  padding: 1rem;
-  font-size: 0.9rem;
-  a {
-    color: $primary-color;
-  }
-}
-
-@keyframes shockwave {
-  0% {
-    transform: scale(1);
-    box-shadow: 0 0 2px $secondary-color;
-    opacity: 1;
-  }
-
-  100% {
-    transform: scale(1);
-    opacity: 0;
-    box-shadow: 0 0 50px darken($secondary-color, 20%),
-      inset 0 0 10px $secondary-color;
   }
 }
 </style>
